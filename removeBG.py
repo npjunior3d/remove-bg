83080b74b24a96f0cc2508d96b347d4db5ce1e08
Ricardo Roson <54441794+rroson@users.noreply.github.com>
2024-06-04 13:48:32 -0300

    Add files via upload

---
+++ removeBG.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from PIL import Image
+from rembg import remove
+
+def remove_bg(imagem, widget):
+    img = Image.open(imagem)
+    sem_fundo = remove(img)
+    widget.title('Sem fundo!')
+    widget.image(sem_fundo)
+
+st.title('Remove fundo de imagens!')
+imagem = st.file_uploader('Escolha uma imagem')
+
+col_a, col_b = st.columns(2)
+
+if imagem:
+    result = st.button(
+        'Remover Fundo',
+        type='primary',
+        on_click=remove_bg,
+        args=(imagem, col_b)
+    )
+    col_a.title('Imagem original')
+    col_a.image(imagem)
+else:
+    st.warning('Ainda não temos uma imagem!')
