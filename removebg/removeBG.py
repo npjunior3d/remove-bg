1804b28427610ab39cc943ae0029651a135aef66
Ricardo Roson <rroson@gmail.com>
2024-09-03 10:41:37 -0300

    Correcao Erro Imagem Nao Suprtada

--- removebg/removeBG.py
+++ removebg/removeBG.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from rembg import remove
+from time import sleep
 
 #'''
 #Este programa remove o plano de fundo de uma imagem utilizando IA
@@ -9,10 +10,19 @@ from rembg import remove
 # Ver como alterar os textos do Widget file_uploader 
 
 def remove_bg(imagem, widget):
-    img = Image.open(imagem)
-    sem_fundo = remove(img)
-    widget.title('Sem fundo!')
-    widget.image(sem_fundo)
+    try:
+        img = Image.open(imagem)
+        sem_fundo = remove(img)
+        widget.title('Sem fundo!')
+        widget.image(sem_fundo)
+    except UnidentifiedImageError:
+        st.error('Imagem não suportada! Tente outra!', icon="🚨")
+        sleep(2)
+        st.stop()
+    except Exception as e:
+        st.error(f"Ocorreu um erro inesperado: {e}", icon="🚨")
+        sleep(2)
+        st.stop()
 
 st.title('Remove fundo de imagens!')
 imagem = st.file_uploader('Professor Ricardo Roson - Colégio Litteratus')
@@ -20,12 +30,13 @@ imagem = st.file_uploader('Professor Ricardo Roson - Colégio Litteratus')
 col_a, col_b = st.columns(2)
 
 if imagem:
-    st.button(
-        'Remover Fundo',
-        type='primary',
-        on_click=remove_bg,
-        args=(imagem, col_b)
-    )
+    # st.button(
+    #     'Remover Fundo',
+    #     type='primary',
+    #     on_click=remove_bg,
+    #     args=(imagem, col_b)
+    # )
+    remove_bg(imagem, col_b)
     col_a.title('Imagem original')
     col_a.image(imagem)
 else:
